6e8920697c7bab57bff24ebfedff0ae881bfec9b
vtj <patricoconnor12@gmail.com>
2022-11-07 21:20:01 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lithos
+Lithos is a header-only library similar in some respects to the popular `boost` headers.
+
+**Note**: Lithos is probably not production ready at all.
---
+++ lithos_test.cpp
@@ -0,0 +1,46 @@
+#include "lithos.hpp"
+#include <string>
+
+int main() {
+  lithos::print("This is a print test.\n");
+  lithos::println("This is a println test.");
+  lithos::printendl("This is a printendl test.");
+  lithos::string test1 = "printfln";
+  lithos::printfln("This is a %s test.", test1);
+  lithos::string test2 = "printfendl";
+  lithos::printfendl("This is a %s test.", test2);
+  std::string t = "test!!";
+  lithos::string test3 = lithos::toStr(t);
+  lithos::printfln(
+    "%s %s %s: %s",
+    "C++ does not like it when you use Lithos strings",
+    "instead of std::strings, so lithos::toStr() should",
+    "fix it because printfln breaks when std::string is used",
+    test3
+  );
+  lithos::assert(1 + 5 == 6);
+  // THIS WILL FAIL
+  // lithos::assert(1 + 5 == 7);
+
+  lithos::KeyPair<lithos::string, lithos::string> kp;
+  kp.set("K val", "T val");
+  lithos::assert((kp.key == "K val" && kp.value == "T val"));
+
+  lithos::KeyPair<lithos::string, lithos::string> kp2("val1", "val2");
+  lithos::assert((kp2.key == "val1" && kp2.value == "val2"));
+
+  lithos::createFile("DELETEME.txt");
+  lithos::removeFile("DELETEME.txt");
+
+  for (int i = 0; i < 10; i++) {
+    printf("%d ", i);
+  }
+  lithos::println();
+
+  int test[4] = {1, 2, 3, 4};
+  foreach(num, test) {
+    lithos::println(num);
+  }
+
+  return 0;
+}
